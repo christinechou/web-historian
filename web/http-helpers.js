be0db8565a1fe54b5306526275835b23c44574ab
christinechou <christinejchou@gmail.com>
2016-06-29 16:57:34 -0700

    Struggle with fs.ReadFile, not returning data

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -7,7 +7,43 @@ exports.headers = {
   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
   'access-control-allow-headers': 'content-type, accept',
   'access-control-max-age': 10, // Seconds.
-  'Content-Type': 'text/html'
+  'Content-Type': 'text/html' // change to application.json?
+};
+
+exports.sendResponse = function(response, data, statusCode) {
+  statusCode = statusCode || 200;
+  console.log('statusCode:', statusCode);
+  response.writeHead(statusCode, exports.headers);
+  response.end(JSON.stringify(data));
+};
+
+exports.collectData = function(request, callback) {
+  var data = '';
+  request.on('data', function(chunk) {
+    data += chunk;
+  });
+  request.on('end', function() {
+    callback(JSON.parse(data));
+  });
+};
+
+exports.makeActionHandler = function(actionMap, request, response) {
+  return exports.sendResponse(request, response);
+
+  // var fcnAction = function(request, response) {
+  //   var action = actionMap[request.method];
+  //   if (action) {
+  //     console.log('execute action:', action);
+  //     action(request, response);
+  //   } else {
+  //     console.log('fail action');
+  //     exports.sendResponse(response, '', 404);
+  //   }
+  // };
+
+  // console.log('action function:', fcnAction);
+
+  // return fcnAction;
 };
 
 exports.serveAssets = function(res, asset, callback) {
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,57 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var httpHelpers = require('../web/http-helpers');
 // require more modules/folders here!
+var fs = require('fs');
+
+var objectIdCounter = 1;
+var messages = [
+  // Note: an initial message is useful for debugging purposes.
+  /*
+  {
+    text: 'hello world',
+    username: 'fred',
+    objectId: objectIdCounter
+  }
+  */
+];
+
+var actions = {
+  'GET': function(request, response) {
+    httpHelpers.sendResponse(response, {results: 'messages'});
+  },
+  'POST': function(request, response) {
+    httpHelpers.collectData(request, function(message) {
+      message.objectId = ++objectIdCounter;
+      messages.push(message);
+      httpHelpers.sendResponse(response, {objectId: 'message.objectId'}, 201);
+    });
+  },
+  'OPTIONS': function(request, response) {
+    httpHelpers.sendResponse(response, null);
+  }
+};
+
+var getHTMLContent = function() {
+  var content = '';
+  fs.readFile(__dirname + '/public/index.html', 'utf-8', function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(data); 
+    content = data;
+  });
+  return content;
+};
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  console.log('request handling method:', req.method);
+  // httpHelpers.makeActionHandler(actions, req, res);
+  var htmlContent = getHTMLContent();
+console.log('content? sure hope so.', htmlContent);
+  res.end(htmlContent);
 };
+
+
+
+
